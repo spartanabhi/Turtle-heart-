ea569561ac8f31e4ada77890138ad199f19582ed
Yashveer Rai <94855959+spartanabhi@users.noreply.github.com>
2022-03-21 21:19:15 +0530

    Add files via upload

---
+++ Turtle trial.py
@@ -0,0 +1,14 @@
+import turtle
+
+x = turtle.Turtle()
+
+x.color("red")
+x.begin_fill( )
+x.pensize(3)
+x.left(50)
+x.forward(133)
+x.circle(50,200)
+x.right(140)
+x.circle(50,200)
+x.forward(133)
+x.end_fill()
